00c31f93f3c4b94346276a35b3297cf917f4a841
mariellelopezz <marielle.lopez@utexas.edu>
2018-03-19 10:10:38 -0500

    Monday Morning Commit

--- src/assignment4/Critter.java
+++ src/assignment4/Critter.java
@@ -1,13 +1,12 @@
 package assignment4;
 /* CRITTERS Critter.java
  * EE422C Project 4 submission by
- * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * Marielle Lopez
+ * mal5427
+ * 15495
+ * Emily Ginsburg
+ * eag3598
+ * 15495
  * Slip days used: <0>
  * Fall 2016
  */
@@ -32,23 +31,23 @@ public abstract class Critter {
 	static {
 		myPackage = Critter.class.getPackage().toString().split(" ")[1];
 	}
-	
+
 	private static java.util.Random rand = new java.util.Random();
 	public static int getRandomInt(int max) {
 		return rand.nextInt(max);
 	}
-	
+
 	public static void setSeed(long new_seed) {
 		rand = new java.util.Random(new_seed);
 	}
-	
-	
+
+
 	/* a one-character long string that visually depicts your critter in the ASCII interface */
 	public String toString() { return ""; }
-	
+
 	private int energy = 0;
 	protected int getEnergy() { return energy; }
-	
+
 	private int x_coord;
 	private int y_coord;
 	private boolean movedThisStep;
@@ -62,13 +61,13 @@ public abstract class Critter {
 	 time step, you must deduct the appropriate energy cost from the critter for walking/running,
 	 but you must not actually alter the critter’s position. Critters can die in this fashion
 	* */
-	
+
 	protected final void walk(int direction) {
 		if(!movedThisStep)
 			this.move(direction, walking_steps);
 		energy -= Params.walk_energy_cost;
 	}
-	
+
 	protected final void run(int direction) {
 		if(!movedThisStep)
 			this.move(direction, running_steps);
@@ -118,22 +117,28 @@ public abstract class Critter {
 
 	}
 
-	
+
 	protected final void reproduce(Critter offspring, int direction) {
-		if(this.getEnergy() <= Params.min_reproduce_energy)
+		if(this.getEnergy() < Params.min_reproduce_energy)
 			return;
 
 		offspring.energy = this.getEnergy() / 2;
-		this.energy = this.getEnergy() / 2; // TODO round up
+		//round parent energy up
+		if( this.getEnergy() % 2 != 0)
+			this.energy = (this.getEnergy() / 2) + 1;
+		else this.energy = this.getEnergy() / 2;
+
 
 		offspring.walk(direction);
 		babies.add(offspring);
+
+		movedThisStep = false;
 
 	}
 
 	public abstract void doTimeStep();
 	public abstract boolean fight(String oponent);
-	
+
 	/**
 	 * create and initialize a Critter subclass.
 	 * critter_class_name must be the unqualified name of a concrete subclass of Critter, if not,
@@ -164,7 +169,7 @@ public abstract class Critter {
 
 
 	}
-	
+
 	/**
 	 * Gets a list of critters of a specific type.
 	 * @param critter_class_name What kind of Critter is to be listed.  Unqualified class name.
@@ -173,10 +178,10 @@ public abstract class Critter {
 	 */
 	public static List<Critter> getInstances(String critter_class_name) throws InvalidCritterException {
 		List<Critter> result = new java.util.ArrayList<Critter>();
-	
+
 		return result;
 	}
-	
+
 	/**
 	 * Prints out how many Critters of each type there are on the board.
 	 * @param critters List of Critters.
@@ -198,13 +203,13 @@ public abstract class Critter {
 			System.out.print(prefix + s + ":" + critter_count.get(s));
 			prefix = ", ";
 		}
-		System.out.println();		
+		System.out.println();
 	}
-	
-	/* the TestCritter class allows some critters to "cheat". If you want to 
+
+	/* the TestCritter class allows some critters to "cheat". If you want to
 	 * create tests of your Critter model, you can create subclasses of this class
-	 * and then use the setter functions contained here. 
-	 * 
+	 * and then use the setter functions contained here.
+	 *
 	 * NOTE: you must make sure that the setter functions work with your implementation
 	 * of Critter. That means, if you're recording the positions of your critters
 	 * using some sort of external grid or some other data structure in addition
@@ -215,29 +220,29 @@ public abstract class Critter {
 		protected void setEnergy(int new_energy_value) {
 			super.energy = new_energy_value;
 		}
-		
+
 		protected void setX_coord(int new_x_coord) {
 
 			CritterWorld.grid[getX_coord()][getY_coord()] = null;
 			CritterWorld.grid[new_x_coord][getY_coord()] = this;
 			super.x_coord = new_x_coord;
 		}
-		
+
 		protected void setY_coord(int new_y_coord) {
 
 			CritterWorld.grid[getX_coord()][getY_coord()] = null;
 			CritterWorld.grid[getX_coord()][new_y_coord] = this;
 			super.y_coord = new_y_coord;
 		}
-		
+
 		protected int getX_coord() {
 			return super.x_coord;
 		}
-		
+
 		protected int getY_coord() {
 			return super.y_coord;
 		}
-		
+
 
 		/*
 		 * This method getPopulation has to be modified by you if you are not using the population
@@ -247,11 +252,11 @@ public abstract class Critter {
 		protected static List<Critter> getPopulation() {
 			return CritterWorld.world;
 		}
-		
+
 		/*
 		 * This method getBabies has to be modified by you if you are not using the babies
 		 * ArrayList that has been provided in the starter code.  In any case, it has to be
-		 * implemented for grading tests to work.  Babies should be added to the general population 
+		 * implemented for grading tests to work.  Babies should be added to the general population
 		 * at either the beginning OR the end of every timestep. EDIT: in use
 		 */
 		protected static List<Critter> getBabies() {
@@ -266,35 +271,31 @@ public abstract class Critter {
 		for(Critter c : CritterWorld.world)
 			CritterWorld.removeFromWorld(c, c.x_coord, c.y_coord);
 	}
-	
+
 	public static void worldTimeStep() throws InvalidCritterException {
 
 		// TODO
-	// 1. increment timestep; timestep++;
-	// 2. doTimeSteps();
-	// 3. Do the fights. doEncounters();
-	// 4. updateRestEnergy();
-	// 5. Generate Algae genAlgae();
-	// 6. Move babies to general population. population.addAll(babies); babies.clear();
+		// 1. increment timestep; timestep++;
+		// 2. doTimeSteps();
+		// 3. Do the fights. doEncounters();
+		// 4. updateRestEnergy();
+		// 5. Generate Algae genAlgae();
+		// 6. Move babies to general population. population.addAll(babies); babies.clear();
 
-		for(Critter c: CritterWorld.world)
-		{
+		for(Critter c: CritterWorld.world) {
 			c.movedThisStep = false;
 			c.doTimeStep();
 		}
-
 		resolveEncounters();
 
 		// remove dead critters from world
-		for(Critter c: CritterWorld.world)
-		{
+		for(Critter c: CritterWorld.world) {
 			c.energy -= Params.rest_energy_cost;	// every critter loses rest energy
 			if(c.getEnergy() <= 0)
 				CritterWorld.removeFromWorld(c, c.x_coord, c.y_coord);                                                                              ;
 		}
 
-		for(int i = 0; i < Params.refresh_algae_count; i++)
-		{
+		for(int i = 0; i < Params.refresh_algae_count; i++) {
 			makeCritter("Algae");
 		}
 
@@ -307,7 +308,7 @@ public abstract class Critter {
 
 
 	}
-	
+
 	public static void displayWorld() {
 		int i; // used in for loops
 		System.out.print("+");
@@ -338,26 +339,20 @@ public abstract class Critter {
 	// returns arraylist with critters that are found in the same position (all on one)
 	// figure out a way so that this only ends when all encounters are found?
 	private static void resolveEncounters() {
-
-		for(Critter c1 : CritterWorld.world)
-		{
-			for(Critter c2 : CritterWorld.world)
-			{
-				if((c1.x_coord == c2.x_coord) && (c1.y_coord == c2.y_coord))	// encounter found
-				{
+		for(Critter c1 : CritterWorld.world) {
+			for(Critter c2 : CritterWorld.world) {
+				if((c1.x_coord == c2.x_coord) && (c1.y_coord == c2.y_coord)){	// encounter found
 					whoGetsToStay(c1, c2);
 				}
 			}
 		}
-
-
-
 	}
 
 	// passes in an arraylist of critters who are on the same location
 	// at the return, the loser is dead (enery of zero, but still present in world)
 	// loser will be removed from world during time step
 	private static void whoGetsToStay(Critter A, Critter B) {
+		Critter winner, loser;
 		if(A.energy == 0 || B.energy == 0) // one critter is already dead, no fight needed
 			return;
 
@@ -365,32 +360,40 @@ public abstract class Critter {
 		boolean BwantsToFight = B.fight(A.toString());
 
 		if((A.energy != 0) && (B.energy != 0)) // neither critter has died attempting to run
-			if ((A.x_coord == B.x_coord) && (A.y_coord == B.y_coord))	// neither critter has ran away
-			{
+			if ((A.x_coord == B.x_coord) && (A.y_coord == B.y_coord)){	// neither critter has run away
+
 				// fight occurs
-				int diceA, diceB;
-				Critter winner, loser;
-
-				if(AwantsToFight)
-					diceA = getRandomInt(A.energy);
-				else
-					diceA = 0;
-
-				if(BwantsToFight)
-					diceB = getRandomInt(B.energy);
-				else
-					diceB = 0;
-
-
-				if(diceA >= diceB)	// A wins (if they are equal, A also wins)
-				{
+				//dementors always wins (but they don't fight Algae)
+				if(A.toString().equals("D") && AwantsToFight){
 					winner = A;
 					loser = B;
-				}
-				else	// B wins
-				{
+				}else
+				if(B.toString().equals("D") && BwantsToFight){
 					winner = B;
 					loser = A;
+					//TODO: if algae and dementor, make sure neither is affected in fight (both stay on map unless energy is drained)
+				//dementor is not involved
+				}else {
+					int diceA, diceB;
+					//set dice
+					//A
+					if (AwantsToFight)
+						diceA = getRandomInt(A.energy);
+					else
+						diceA = 0;
+					//B
+					if (BwantsToFight)
+						diceB = getRandomInt(B.energy);
+					else
+						diceB = 0;
+					//compare the dice
+					if (diceA >= diceB){    // A wins (if they are equal, A also wins)
+						winner = A;
+						loser = B;
+					} else {   			    // B wins
+						winner = B;
+						loser = A;
+					}
 				}
 
 				winner.energy += (loser.energy / 2);
