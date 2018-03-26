0141e415928627f9f6b14f4c381ed94e86d5de3f
mariellelopezz <marielle.lopez@utexas.edu>
2018-03-26 12:25:00 -0500

    commit

--- src/assignment4/Monkey.java
+++ src/assignment4/Monkey.java
@@ -1,6 +1,6 @@
 package assignment4;
 
-/* CRITTERS Critter.java
+/* CRITTERS Monkey.java
  * EE422C Project 4 submission by
  * Marielle Lopez
  * mal5427
@@ -11,48 +11,59 @@ package assignment4;
  * Slip days used: <0>
  * Spring 2018
  */
-//Monkeys
-//Time Step:
-//          Monkeys reproduce every 3 Steps
-//          Monkeys run otherwise.
-//Fight:
-//          Monkeys always fight, unless it is against a dementor in which case, they run away
-//          The amount of Algae a monkey eats is always counted
-//Run Stats:
-//          The amount of Monkeys present is reported
-//          The amount of Algae encountered and eaten by Monkeys is reported
 
+/**
+ * Monkeys (class made by mal5427)
+ * Time Step:
+ *          Monkeys reproduce every 3 Steps
+ *          Monkeys run otherwise.
+ * Fight:
+ *          Monkeys always fight, unless it is against a dementor in which case, they run away
+ *          The amount of Algae a monkey eats is always counted
+ * Run Stats:
+ *          The amount of Monkeys present is reported
+ *          The amount of Algae encountered and eaten by Monkeys that are alive is reported
+ */
 public class Monkey extends Critter{
 
     private int dir;
-    private int reproductionCount;
+    private int moves;
     private int algaeEaten;
 
-
+    /**
+     * Monkey() is the constructor. direction is set to a random number max 8.
+     */
     Monkey(){
         dir = Critter.getRandomInt(8);  //get direction
     }
 
+    /**
+     * toString() overrides for this class.
+     * @return returns a string representation of the Emily.
+     */
     @Override
     public String toString() { return "M"; }
 
     @Override
     public void doTimeStep() {
+        moves++;
         //reproduces every 3 time steps
-        if(reproductionCount % 3 == 0){
+        if(moves % 3 == 0){
             Monkey child = new Monkey();
             reproduce(child, Critter.getRandomInt(8));
-            reproductionCount++;
         }else{
             run(dir);
         }
 
     }
 
+    /**
+     * fight() monkey always fights unless it encounters a dementor which it ignores
+     * @param oponent is the critter they encounters
+     * @return returns boolean true if opponent is not a dementor and true otherwise
+     */
     @Override
     public boolean fight(String oponent) {
-        //monkey is scared of dementors
-        //  TODO: implement running away
         if(oponent.equals("D"))
             return false;
         //count algae encounters
@@ -61,13 +72,18 @@ public class Monkey extends Critter{
         return true;
     }
 
-    public static void runStats(java.util.List<Critter> monkeys){
+    /**
+     * runStats() compiles and outputs the statistics of the class that it is called on.
+     * @param critters is the list of Critters (precondition: the passed list must contain
+     *                 Critters of the same kind that it is called on)
+     */
+    public static void runStats(java.util.List<Critter> critters){
         int totalAlgaeEaten = 0;
-        for(Critter mon : monkeys){
+        for(Critter mon : critters){
             Monkey monkey = (Monkey) mon;
             totalAlgaeEaten += monkey.algaeEaten;
         }
-        System.out.print("" + monkeys.size() + " total Monkeys,    ");
+        System.out.print("" + critters.size() + " total Monkeys,    ");
         System.out.print("" + totalAlgaeEaten + " total Algae Eaten by Monkeys    ");
         System.out.println();
     }
