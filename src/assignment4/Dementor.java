ca53dce1bce0e447ac4264099f2a0656822c95a6
mariellelopezz <marielle.lopez@utexas.edu>
2018-03-26 12:27:22 -0500

    commit

--- src/assignment4/Dementor.java
+++ src/assignment4/Dementor.java
@@ -1,5 +1,5 @@
 package assignment4;
-/* CRITTERS Critter.java
+/* CRITTERS Dementor.java
  * EE422C Project 4 submission by
  * Marielle Lopez
  * mal5427
@@ -10,67 +10,90 @@ package assignment4;
  * Slip days used: <0>
  * Spring 2018
  */
-//Dementors
-//Time Step:
-//          Dementors change direction counter clockwise every 5 Steps
-//          Dementors reproduce every 7 steps
-//          Dementors run otherwise
-//Fight:
-//          Dementors always fight, unless it is against Algae, in which case they ignore the algae
-//Report Stats:
-//          The amount of Dementors is reported
-//          The amount of Kills by Dementors is reported
-
+/**
+ * Dementors (class made by mal5427)
+ * Time Step:
+ *          Dementors change direction counter clockwise every 5 Steps
+ *          Dementors reproduce every 7 steps
+ *          Dementors run otherwise
+ * Fight:
+ *          Dementors always fight.
+ * Report Stats:
+ *          The amount of Dementors is reported
+ *          The amount of Kills by Dementors that remain alive is reported
+ */
 public class Dementor extends Critter{
 
     private int dir;
-    //TODO: what happens when dementor dies? How can I keep the amount of kills it had while still removing it from the world?
-    //add STATIC modifier to kills variable so all the same?
-    //TODO: Same problem in Monkey
-    private int kills = 0;
+    private static int kills;
     private int moves;
-    private int reproductionCount;
 
+    /**
+     * Dementor() is the constructor. Sets direction to a random number out of 8. sets kill count and move count to 0.
+     */
     Dementor(){
         dir = Critter.getRandomInt(8);
         moves = 0;
-        reproductionCount = 0;
+        kills = 0;
     }
+
+    /**
+     * getKills() is the accessor for the kills counter
+     * @return
+     */
     private int getKills(){
         return kills;
     }
 
+    /**
+     * toString() overrides for this class.
+     * @return returns a string representation of the Emily.
+     */
     @Override
-    public String toString() { return "D"; }
+    public String toString() {
+        return "D" + dir + "|" + getEnergy(); }
 
+    /**
+     * doTimeStep() updates the Dementor's Behavior.
+     * dementor changes direction every 5 steps and reproduces every 7 steps
+     */
     @Override
     public void doTimeStep() {
+        moves++;
         //changes direction every 5 steps
         if(moves % 5 == 0)
             dir++;
         //reproduce every 7 steps
-        if(reproductionCount % 7 == 0){
+        if(moves % 7 == 0){
             Dementor child = new Dementor();
             reproduce(child, Critter.getRandomInt(8));
         }else run(dir);
     }
 
+    /**
+     * fight() always returns true. kill count is incremented. the input string is never used.
+      * @param not_used
+     * @return returns true
+     */
     @Override
-    public boolean fight(String oponent) {
-        //does not fight algae only
-        if(oponent.equals("@"))
-            return false;
+    public boolean fight(String not_used) {
+        kills++;
         return true;            //Dementors fight everything else
     }
 
-    public static void runStats(java.util.List<Critter> dementors) {
+    /**
+     * runStats() compiles and outputs the statistics of the class that it is called on.
+     * @param critters is the list of Critters (precondition: the passed list must contain
+     *                 Critters of the same kind that it is called on)
+     */
+    public static void runStats(java.util.List<Critter> critters) {
         int totalKills = 0;
-        for(Critter dem : dementors){
+        for(Critter dem : critters){
             Dementor dementor = (Dementor) dem;
             totalKills += dementor.getKills();
         }
 
-        System.out.print("" + dementors.size() + " total Dementors,    ");
+        System.out.print("" + critters.size() + " total Dementors,    ");
         System.out.print("" + totalKills + " total kills by Dementors    ");
         System.out.println();
     }
